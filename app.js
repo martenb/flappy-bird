e1d046e9fa262f287a09495f160f3e4e0e781667
Pavel Pivoda <pavel.pivoda@adbros.cz>
2024-10-14 17:33:58 +0200

    feat: start/restart, fix obstacle glitches, make bird fly

--- app.js
+++ app.js
@@ -1,8 +1,10 @@
 document.addEventListener('DOMContentLoaded' , () => {
     const score = document.querySelector('.score')
     const bird = document.querySelector('.bird')
+    const birdWings = bird.querySelector('img')
     const gameDisplay = document.querySelector('.game-container')
-    const ground = document.querySelector('.ground-moving')
+    const ground = document.querySelector('.ground')
+    console.log("%cPodvádět se nemá!", "color: #b51982; font-size: xx-large")
 
     let birdLeft = 220
     let birdBottom = 100
@@ -10,28 +12,48 @@ document.addEventListener('DOMContentLoaded' , () => {
     let isGameOver = false
     let gap = 430
     let points = 0
+    let gameTimerId = undefined
 
 
     function startGame() {
+        document.body.focus()
         birdBottom -= gravity
         bird.style.bottom = birdBottom + 'px'
         bird.style.left = birdLeft + 'px'
     }
-    let gameTimerId = setInterval(startGame, 20)
+
+    document.addEventListener('keyup', controlStart)
+    function controlStart(e) {
+        if (e.keyCode === 32) {
+            begin()
+            document.removeEventListener('keyup', controlStart)
+        }
+    }
+
+    function controlEnd(e) {
+        if (e.keyCode === 32 && e.ctrlKey) {
+            restart()
+            document.removeEventListener('keyup', controlEnd)
+        }
+    }
 
     function control(e) {
         if (e.keyCode === 32) {
             jump()
         }
     }
+    
 
     function jump() {
-        if (birdBottom < 500) birdBottom += 50
+        if (birdBottom < 500) {
+            birdBottom += 50
+            birdWings.classList.add('wing-flap')
+            setTimeout(() => {
+                birdWings.classList.remove('wing-flap')
+            }, 166)
+        }
         bird.style.bottom = birdBottom + 'px'
-        console.log(birdBottom)
     }
-    document.addEventListener('keyup', control)
-
 
     function generateObstacle() {
         let obstacleLeft = 500
@@ -51,6 +73,10 @@ document.addEventListener('DOMContentLoaded' , () => {
         topObstacle.style.bottom = obstacleBottom + gap + 'px'
 
         function moveObstacle() {
+            if (isGameOver) {
+                clearInterval(timerId)
+                return
+            }
             obstacleLeft -=2
             obstacle.style.left = obstacleLeft + 'px'
             topObstacle.style.left = obstacleLeft + 'px'
@@ -69,27 +95,54 @@ document.addEventListener('DOMContentLoaded' , () => {
             if (
                 obstacleLeft > 200 && obstacleLeft < 280 && birdLeft === 220 &&
                 (birdBottom < obstacleBottom + 153 || birdBottom > obstacleBottom + gap -200)||
-                birdBottom === 0 
+                birdBottom <= 0 
                 ) {
-                gameOver()
                 clearInterval(timerId)
+                gameOver()
             }
         }
+
         let timerId = setInterval(moveObstacle, 20) 
-        if (!isGameOver) setTimeout(generateObstacle, 3000)
+        
+        if (!isGameOver) {
+            setTimeout(generateObstacle, 3000)
+        }
 
     }
-    generateObstacle()
 
+    function begin () {
+      document.body.classList.remove('game--start')
+      document.addEventListener('keyup', control)
+      ground.classList.add('ground-moving')
+      ground.classList.remove('ground')
+      gameTimerId = setInterval(startGame, 20)
+      generateObstacle()
+    }
 
     function gameOver() {
+        isGameOver = true
         clearInterval(gameTimerId)
         console.log('game over')
-        isGameOver = true
         document.removeEventListener('keyup', control)
         ground.classList.add('ground')
         ground.classList.remove('ground-moving')
+        end()
     }
 
+    function end () {
+      const finalScore = document.querySelector('.game-over-score')
+      const finalPoints = points
+      finalScore.innerHTML = finalPoints
+      setInterval(() => {
+        const finalScore = document.querySelector('.game-over-score')
+        finalScore.innerHTML = finalPoints
+      }, 100)
+      document.body.classList.add('game--end')
+      document.addEventListener('keyup', controlEnd)
+    }
 
+    function restart() {
+        window.location.reload()
+    }
 })
+   
